b50edcd725f5738635368767e1f54f0c985f174a
Vandana Rajan <vandurajan91@gmail.com>
2020-06-12 15:07:56 +0100

    Update vae_audio.py

--- src/models/vae_audio.py
+++ src/models/vae_audio.py
@@ -18,5 +18,28 @@ hidden_dim = 64
 
 # Classes
 class Enc(nn.Module):
-  
+  """ Generate latent parameters for RECOLA audio arousal features. """
+  def __init__(self, latent_dim):
+    super(Enc, self).__init__()
+    self.linear1 = nn.Linear(data_dim, hidden_dim)
+    self.enc = nn.Linear(hidden_dim, latent_dim)
+    self.fc21 = nn.Linear(latent_dim,latent_dim)
+    self.fc22 = nn.Linear(latent_dim,latent_dim)
+  def forward(self,x):
+    e = self.enc(self.linear1(x))  # x.shape=(batch_size,88)
+    lv = self.fc22(e)
+    return self.fc21(e), F.softmax(lv, dim=-1) * lv.size(-1) + Constants.eta
+ 
+class Dec(nn.Module):
+    """ Generate RECOLA audio arousal feature given a sample from the latent space. """
+    def __init__(self, latent_dim):
+      super(Dec, self).__init__()
+      self.dec = nn.Linear(latent_dim, hidden_dim)
+      self.fc3 = nn.Linear(hidden_dim, data_dim)
+    def forward(self, z):
+      p = self.fc3(self.dec(z))
+      
+      
+
+    
 
